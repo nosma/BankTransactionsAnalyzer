a61789f94fec55641aa704b69c3d1803ac2338c2
Manos <fragmanos@gmail.com>
2015-12-01 00:31:58 +0000

    Voice recognition functionality added

--- src/main/resources/static/scripts/config/route.js
+++ src/main/resources/static/scripts/config/route.js
@@ -1,16 +1,19 @@
 'use strict';
 
 app.config(function ($routeProvider) {
-  $routeProvider.
-    when('/Transactions', {
-      templateUrl: 'views/transactions.html',
-      controller: 'transactions'
-    }).when('/Statistics', {
-      templateUrl: 'views/statistics.html'
-      //controller: 'statistics'
-    }).when('/Home', {
-      templateUrl: 'views/home.html'
-    }).otherwise({
-      templateUrl: 'views/home.html'
-    });
+    $routeProvider.
+        when('/Transactions', {
+            templateUrl: 'views/transactions.html',
+            controller: 'transactions'
+        }).when('/Statistics', {
+            templateUrl: 'views/statistics.html',
+            controller: 'statistics'
+        }).when('/Home', {
+            templateUrl: 'views/home.html'
+        }).when('/Microphone', {
+            templateUrl: 'views/microphone.html',
+            controller: 'VoiceCtrl'
+        }).otherwise({
+            templateUrl: 'views/home.html'
+        });
 });
