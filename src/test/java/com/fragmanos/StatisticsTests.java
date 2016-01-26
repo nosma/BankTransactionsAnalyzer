77dfa572733cc8417f86312c81f574fbd3873c61
Manos <fragmanos@gmail.com>
2016-01-26 15:54:56 +0000

    Statistic test file forgotten by previous commit

--- src/test/java/com/fragmanos/StatisticsTests.java
+++ src/test/java/com/fragmanos/StatisticsTests.java
@@ -1,101 +1,99 @@
 package com.fragmanos;
 
-import com.fragmanos.controller.BankTransactionUtil;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fragmanos.database.dao.BankTransactionDao;
 import com.fragmanos.database.dao.MonthStatDao;
 import com.fragmanos.database.model.BankTransaction;
-import com.fragmanos.directory.DirectoryReader;
-import com.fragmanos.web.controller.BankStatisticsController;
 import com.fragmanos.web.controller.BankStatisticsService;
+import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class StatisticsTests {
 
   private static final double DOUBLE_DELTA = 1e-15;
 
-  private String INPUT_DIRECTORY;
-  private DirectoryReader directoryReader;
-  private BankTransactionUtil bankTransactionUtil;
-
   BankStatisticsService bankStatisticsService;
 
-  @Autowired
-  private BankStatisticsController bankStatisticsController;
+  private MonthStatDao monthStatDaoMock;
+  private BankTransactionDao bankTransactionDaoMock;
+  private List<BankTransaction> bankTransactionList;
 
-  @Autowired
-  private List<BankTransaction> bankTransactionsFromDirectory;
-
-  @Autowired
-  private MonthStatDao monthStatDao;
-
-  @Autowired
-  private BankTransactionDao bankTransactionDao;
-
-  //TODO fix the failing tests based one the new application design
   @Before
   public void setUp() throws Exception {
-    INPUT_DIRECTORY = System.getProperty("user.dir") + "/src/test/resources/testTransactions/";
-    directoryReader = new DirectoryReader();
-    bankTransactionUtil = new BankTransactionUtil();
-    bankTransactionsFromDirectory = bankTransactionUtil.getBankTransactionsFromDirectory(INPUT_DIRECTORY);
-    bankStatisticsService = new BankStatisticsService(monthStatDao,bankTransactionDao);
-    bankStatisticsController = new BankStatisticsController(bankStatisticsService);
+    monthStatDaoMock = mock(MonthStatDao.class);
+    bankTransactionDaoMock = mock(BankTransactionDao.class);
+    bankStatisticsService = new BankStatisticsService(monthStatDaoMock, bankTransactionDaoMock);
+
+    bankTransactionList = new ArrayList<BankTransaction>();
+    when(bankTransactionDaoMock.findAllByOrderByTransactiondateDesc()).thenReturn(bankTransactionList);
   }
 
   /* Total calculations */
   @Test
   public void testTotalIncomeMatchesExpected() throws Exception {
-    assertEquals(12288.0, bankStatisticsController.getTotalIncome(), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(), "TEST", 12288.0));
+    assertEquals(12288.0, bankStatisticsService.getTotalIncome(), DOUBLE_DELTA);
   }
 
   @Test
   public void testTotalExpensesMatchesExpected() throws Exception {
-    assertEquals(-3335.0, bankStatisticsController.getTotalExpenses(), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(), "TEST", -3335.0));
+    assertEquals(-3335.0, bankStatisticsService.getTotalExpenses(), DOUBLE_DELTA);
   }
 
   @Test
   public void testTotalProfitMatchesExpected() throws Exception {
-    double profit = bankStatisticsController.getTotalIncome() + bankStatisticsController.getTotalExpenses();
+    bankTransactionList.add(new BankTransaction(new LocalDate(), "TEST1", 12288.0));
+    bankTransactionList.add(new BankTransaction(new LocalDate(), "TEST2", -3335.0));
+    double profit = bankStatisticsService.getTotalIncome() + bankStatisticsService.getTotalExpenses();
     assertEquals(8953.0, profit, DOUBLE_DELTA);
   }
 
   /* Yearly calculations */
   @Test
   public void testYearlyIncomeMatchesExpected() throws Exception {
-    assertEquals(12288.0, bankStatisticsController.getYearlyIncome(2015), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 1, 1), "TEST", 12288.0));
+    assertEquals(12288.0, bankStatisticsService.getYearlyIncome(2015), DOUBLE_DELTA);
   }
 
   @Test
   public void testYearlyExpensesMatchesExpected() throws Exception {
-    assertEquals(-3335.0, bankStatisticsController.getYearlyExpenses(2015), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 1, 1), "TEST", -3335.0));
+    assertEquals(-3335.0, bankStatisticsService.getYearlyExpenses(2015), DOUBLE_DELTA);
   }
 
   @Test
   public void testYearlyProfitMatchesExpected() throws Exception {
-    double yearlyProfit = bankStatisticsController.getYearlyIncome(2015) + bankStatisticsController.getYearlyExpenses(2015);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 1, 1), "TEST1", 12288.0));
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 2, 1), "TEST2", -3335.0));
+    double yearlyProfit = bankStatisticsService.getYearlyIncome(2015) + bankStatisticsService.getYearlyExpenses(2015);
     assertEquals(8953.0, yearlyProfit, DOUBLE_DELTA);
   }
 
   /* Monthly calculations */
   @Test
   public void testMonthlyIncomeMatchesExpected() throws Exception {
-    assertEquals(4096.0, bankStatisticsController.getMonthlyIncome(10, 2015), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 10, 31), "TEST", 4096.0));
+    assertEquals(4096.0, bankStatisticsService.getMonthlyIncome(10, 2015), DOUBLE_DELTA);
   }
 
   @Test
   public void testMonthlyExpensesMatchesExpected() throws Exception {
-    assertEquals(-1612.0, bankStatisticsController.getMonthlyExpenses(10, 2015), DOUBLE_DELTA);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 10, 31), "TEST", -1612.0));
+    assertEquals(-1612.0, bankStatisticsService.getMonthlyExpenses(10, 2015), DOUBLE_DELTA);
   }
 
   @Test
   public void testMonthlyProfitMatchesExpected() throws Exception {
-    double monthlyProfit = bankStatisticsController.getMonthlyIncome(10, 2015) + bankStatisticsController.getMonthlyExpenses(10, 2015);
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 10, 31), "TEST1", 4096.0));
+    bankTransactionList.add(new BankTransaction(new LocalDate(2015, 10, 31), "TEST2", -1612.0));
+    double monthlyProfit = bankStatisticsService.getMonthlyIncome(10, 2015) + bankStatisticsService.getMonthlyExpenses(10, 2015);
     assertEquals(2484.0, monthlyProfit, DOUBLE_DELTA);
   }
 }
