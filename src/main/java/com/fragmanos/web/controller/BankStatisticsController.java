5a6ede645293a2117cb077e9cc3dec57a73e2e54
Manos <fragmanos@gmail.com>
2016-01-26 15:48:01 +0000

    Mocking statistic tests

--- src/main/java/com/fragmanos/web/controller/BankStatisticsController.java
+++ src/main/java/com/fragmanos/web/controller/BankStatisticsController.java
@@ -1,58 +1,53 @@
 package com.fragmanos.web.controller;
 
-import com.fragmanos.database.model.BankTransaction;
+import java.util.List;
+
 import com.fragmanos.database.model.MonthStat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/statistics/")
 public class BankStatisticsController {
 
-  private final BankStatisticsService bankStatisticsService;
-
   @Autowired
-  public BankStatisticsController(BankStatisticsService bankStatisticsService) {
-    this.bankStatisticsService = bankStatisticsService;
-  }
+  private BankStatisticsInterface bankStatisticsInterface;
 
   @RequestMapping(value = "totalIncome")
   public double getTotalIncome() {
-    return bankStatisticsService.getTotalIncome();
+    return bankStatisticsInterface.getTotalIncome();
   }
 
   @RequestMapping(value = "totalExpenses")
   public double getTotalExpenses() {
-    return bankStatisticsService.getTotalExpenses();
+    return bankStatisticsInterface.getTotalExpenses();
   }
 
   @RequestMapping("monthly")
   public List<MonthStat> getMonthlyStatistics(){
-    return bankStatisticsService.getMonthlyStatistics();
+    return bankStatisticsInterface.getMonthlyStatistics();
   }
 
   @RequestMapping(value = "monthlyIncome/{yearNumber}/{monthNumber}")
   public double getMonthlyIncome(@PathVariable int monthNumber, @PathVariable int yearNumber) {
-    return  bankStatisticsService.getMonthlyIncome(monthNumber, yearNumber);
+    return  bankStatisticsInterface.getMonthlyIncome(monthNumber, yearNumber);
   }
 
   @RequestMapping(value = "monthlyExpenses/{yearNumber}/{monthNumber}")
   public double getMonthlyExpenses(@PathVariable int monthNumber, @PathVariable int yearNumber) {
-    return bankStatisticsService.getMonthlyExpenses(monthNumber, yearNumber);
+    return bankStatisticsInterface.getMonthlyExpenses(monthNumber, yearNumber);
   }
 
   @RequestMapping(value = "yearlyExpenses/{yearNumber}")
   public double getYearlyExpenses(@PathVariable int yearNumber) {
-    return bankStatisticsService.getYearlyExpenses(yearNumber);
+    return bankStatisticsInterface.getYearlyExpenses(yearNumber);
   }
 
   @RequestMapping(value = "yearlyIncome/{yearNumber}")
   public double getYearlyIncome(@PathVariable int yearNumber) {
-    return bankStatisticsService.getYearlyIncome(yearNumber);
+    return bankStatisticsInterface.getYearlyIncome(yearNumber);
   }
 
 }
