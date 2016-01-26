4d2e9f23a0e64acf59ba88b14881d54d3893ce56
Manos <fragmanos@gmail.com>
2016-01-26 15:48:01 +0000

    Mocking statistic tests

--- src/main/java/com/fragmanos/web/controller/BankStatisticsService.java
+++ src/main/java/com/fragmanos/web/controller/BankStatisticsService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class BankStatisticsService {
+public class BankStatisticsService implements BankStatisticsInterface {
 
     private final MonthStatDao monthStatDao;
 
