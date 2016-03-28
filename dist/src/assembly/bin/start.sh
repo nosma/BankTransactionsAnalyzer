ff0b4117f519d2ea07d67c0f0fd360fc10a131e0
Manos <fragmanos@gmail.com>
2016-03-28 21:27:24 +0100

    Un-obfuscated version of the jar inside the zip

--- dist/src/assembly/bin/start.sh
+++ dist/src/assembly/bin/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -jar ${project.parent.artifactId}-webApp-${project.version}.jar
+#java -jar ${project.parent.artifactId}-webApp-${project.version}.jar
+java -jar webApp-${project.version}.jar
