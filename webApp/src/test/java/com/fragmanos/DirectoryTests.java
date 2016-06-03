02c49bea10bebc299058ee9e09d8eb8dfc1b4765
Manos <fragmanos@gmail.com>
2016-06-03 21:47:44 +0100

    Refactor test and delete test folder after tests

--- webApp/src/test/java/com/fragmanos/DirectoryTests.java
+++ webApp/src/test/java/com/fragmanos/DirectoryTests.java
@@ -1,22 +1,29 @@
 package com.fragmanos;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.text.ParseException;
-
 import life.directory.DirectoryReader;
 import life.util.BankTransactionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class DirectoryTests {
 
-  private final String INPUT_DIRECTORY = System.getProperty("user.dir") + getTestPath();
+  private static final Logger log = LoggerFactory.getLogger(DirectoryTests.class);
+
+  private final String TEST_FOLDER = "testFolder";
+  private final String TEST_DIRECTORY = System.getProperty("user.dir") + getTestFolderDir();
   private final String FILENAME = "BankTransactions.csv";
   private DirectoryReader directoryReader;
   private BankTransactionUtil bankTransactionUtil;
@@ -26,60 +33,62 @@ public class DirectoryTests {
   public void setUp() throws Exception {
     directoryReader = new DirectoryReader();
     bankTransactionUtil = new BankTransactionUtil();
-    transactionsFile = new File(Paths.get(INPUT_DIRECTORY+File.separator+FILENAME).toString());
+    transactionsFile = new File(Paths.get(TEST_DIRECTORY +File.separator+FILENAME).toString());
     createTestDir();
+    writeToFile(transactionsFile);
   }
 
   @AfterClass
   public void tearDown() throws Exception {
-    //TODO Delete the directory also, not only the file ...
     if (transactionsFile.exists())
-      System.out.println((transactionsFile.delete()) ? "File Deleted." : "File NOT Deleted.");
-  }
-
-  private void createTestDir() {
-    if(!transactionsFile.getParentFile().mkdir()) {
-      try {
-        if(transactionsFile.createNewFile()){
-          fillFile(transactionsFile);
-        }
-      } catch(IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  private void fillFile(File transactionsFile) {
-    try {
-      if (transactionsFile.exists()) {
-        FileWriter writer = new FileWriter(transactionsFile);
-        writer.write("6/30/15,NEXT RETAIL LTD STRATFORD,-30\n" +
-                       "6/30/15,FORMICARY LIMITED SALARY,1462.68\n");
-        writer.flush();
-        writer.close();
-        System.out.println("Transactions file created!");
-      }
-    } catch(IOException e) {
-      e.printStackTrace();
-    }
+      log.debug((transactionsFile.delete()) ? "File Deleted." : "File NOT Deleted.");
+    log.debug((Files.deleteIfExists(Paths.get(TEST_DIRECTORY))) ? "Dir Deleted." : "Dir NOT Deleted.");
   }
 
   @Test
   public void checkIfCSVDirectoryIsEmpty() {
-    assertFalse(directoryReader.isDirectoryEmpty(INPUT_DIRECTORY));
+    assertFalse(directoryReader.isDirectoryEmpty(TEST_DIRECTORY));
   }
 
   @Test(dependsOnMethods = "checkIfCSVDirectoryIsEmpty")
   public void readCSVsFromDirectory() {
-    assertEquals(1, directoryReader.csvScanner(INPUT_DIRECTORY).size());
+    assertEquals(1, directoryReader.csvScanner(TEST_DIRECTORY).size());
   }
 
   @Test(dependsOnMethods = "readCSVsFromDirectory")
   public void readCSVFile() throws IOException, ParseException {
-    assertEquals(2, bankTransactionUtil.getBankTransactionsFromDirectory(INPUT_DIRECTORY).size());
+    assertEquals(2, bankTransactionUtil.getBankTransactionsFromDirectory(TEST_DIRECTORY).size());
   }
 
-  private String getTestPath() {
-    return Paths.get(File.separator+"testDirectory").toString();
+  private String getTestFolderDir() {
+    return Paths.get(File.separator+ TEST_FOLDER).toString();
   }
+
+  private void createTestDir() {
+    try {
+      Files.createDirectory(Paths.get(TEST_DIRECTORY));
+    } catch (IOException e) {
+      log.error("Error while creating test directory\n" + e);
+    }
+  }
+
+  private void writeToFile(File transactionsFile) {
+    try {
+      if (!transactionsFile.exists()) {
+        if(transactionsFile.createNewFile()){
+          FileWriter writer = new FileWriter(transactionsFile);
+          writer.write("6/30/15,NEXT RETAIL LTD STRATFORD,-30\n" +
+                         "6/30/15,FORMICARY LIMITED SALARY,1462.68\n");
+          writer.flush();
+          writer.close();
+          System.out.println("Transactions file created!");
+        } else {
+          log.error("File " + transactionsFile.getName() + " did not created.");
+        }
+      } else { log.warn("File " + transactionsFile.getName() + " already exists."); }
+    } catch(IOException e) {
+      log.error("Error during file creation "+e);
+    }
+  }
+
 }
