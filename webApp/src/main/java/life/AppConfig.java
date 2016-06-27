07472d0340b406b70f749ed8b81979a58e67e1fa
Manos <fragmanos@gmail.com>
2016-06-27 17:33:07 +0100

    Revert changes for war structure

--- webApp/src/main/java/life/AppConfig.java
+++ webApp/src/main/java/life/AppConfig.java
@@ -2,8 +2,6 @@ package life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author fragkakise on 22/09/2015.
@@ -14,12 +12,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  */
 @SpringBootApplication
 
-public class AppConfig extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(AppConfig.class);
-  }
+public class AppConfig  {
 
   public static void main(String[] args) {
     SpringApplication.run(AppConfig.class, args);
