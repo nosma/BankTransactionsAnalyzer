626e145c3f0efa6a749a56522f7fa389cfde20bb
Manos <fragmanos@gmail.com>
2016-08-23 08:43:58 +0100

    YearMonth converter for MonthStat object

---
+++ webApp/src/main/java/life/database/converter/YearMonthAttributeConverter.java
@@ -0,0 +1,26 @@
+package life.database.converter;
+
+import  javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.Calendar;
+
+@Converter(autoApply = true)
+public class YearMonthAttributeConverter implements AttributeConverter<YearMonth, Date> {
+
+  @Override
+  public Date convertToDatabaseColumn(YearMonth yearMonth) {
+    LocalDate localDate = LocalDate.of(yearMonth.getYear(), yearMonth.getMonth(), 1);
+    return (yearMonth == null ? null : Date.valueOf(localDate));
+  }
+
+  @Override
+  public YearMonth convertToEntityAttribute(Date sqlDate) {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(sqlDate);
+    YearMonth yearMonth = YearMonth.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1);
+    return (sqlDate == null ? null : yearMonth);
+  }
+}
