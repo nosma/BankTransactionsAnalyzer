50e14fdb1b1cd1770f412f8efbbec163e731682d
Manos <fragmanos@gmail.com>
2016-12-13 20:15:39 +0000

    Midata functionality to uploader

--- webApp/src/main/java/life/web/controller/UploadController.java
+++ webApp/src/main/java/life/web/controller/UploadController.java
@@ -1,40 +1,84 @@
 package life.web.controller;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.List;
-
 import life.database.model.BankTransaction;
+import life.database.model.MidataTransaction;
 import life.file.parser.FileParser;
-import org.springframework.beans.factory.annotation.Autowired;
+import life.file.parser.MidataCsvParser;
+import life.file.parser.StatementCsvParser;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.inject.Inject;
+import java.io.*;
+import java.text.ParseException;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/upload/")
+@RequestMapping("/api/upload")
 public class UploadController {
 
+  private Logger log = Logger.getLogger(UploadController.class.getName());
+
+  @Value("${transactions.directory}")
+  public String inputDirectory;
+
+  private static final String STATEMENT = "statement";
+  private static final String MIDATA = "midata";
   private FileParser fileParser;
   private BankingFacade bankingFacade;
 
-  @Autowired
+  @Inject
   public UploadController(FileParser fileParser, BankingFacade bankingFacade) {
     this.fileParser = fileParser;
     this.bankingFacade = bankingFacade;
   }
 
-  @RequestMapping(value = "transactions", method = RequestMethod.POST)
+  @RequestMapping(value = "/transactions", method = RequestMethod.POST)
   @ResponseBody
   public boolean uploadTransactions(@RequestParam("file") MultipartFile multipartFile) throws IOException, ParseException {
-    if (fileParser.canParse(multipartFile)) {
-      final long start = System.currentTimeMillis();
-      List<BankTransaction> bankTransactions = (List<BankTransaction>) fileParser.parse(multipartFile);
-      System.out.println("Parse file " + multipartFile.getOriginalFilename() + "Elapsed " + (System.currentTimeMillis() - start) + " ms");
-      bankingFacade.save(bankTransactions);
-      return true;
+    File file = getFileFromMultipart(multipartFile);
+    if (fileParser.canParse(file)) {
+      return fileUploadRouter(file);
     }
-
     throw new UnsupportedOperationException("File type not support: " + multipartFile.getOriginalFilename());
+  }
+
+  private boolean fileUploadRouter(File uploadedFile) throws IOException, ParseException {
+    String fileInLowerCase = uploadedFile.getName().toLowerCase();
+    boolean uploaded = false;
+    if (fileInLowerCase.contains(STATEMENT)) {
+      uploaded = saveStatements(uploadedFile);
+    } else if (fileInLowerCase.contains(MIDATA)) {
+      uploaded = saveMidata(uploadedFile);
+    }
+    return uploaded;
+  }
+
+  private boolean saveMidata(File file) throws IOException, ParseException {
+    List<MidataTransaction> midataTransactions = new MidataCsvParser().parse(file);
+    bankingFacade.saveMidata(midataTransactions);
+    return midataTransactions.size() > 0;
+  }
+
+  private boolean saveStatements(File file) throws IOException, ParseException {
+    List<BankTransaction> bankTransactions = new StatementCsvParser().parse(file);
+    bankingFacade.saveTransactions(bankTransactions);
+    return bankTransactions.size() > 0;
+  }
+
+  public File getFileFromMultipart(MultipartFile multipartFile) {
+    String pathname = multipartFile.getOriginalFilename();
+    FileOutputStream outputStream;
+    try {
+      outputStream = new FileOutputStream(pathname);
+      outputStream.write(multipartFile.getBytes());
+      outputStream.close();
+    } catch (IOException e) {
+      log.error("", e);
+    }
+    return new File(pathname);
   }
 
 }
---
+++ webApp/src/test/java/life/web/controller/UploadControllerTest.java
@@ -0,0 +1,41 @@
+package life.web.controller;
+
+import life.file.parser.MidataCsvParser;
+import org.springframework.mock.web.MockMultipartFile;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertTrue;
+
+public class UploadControllerTest {
+
+  @Inject
+  private UploadController uploadController;
+  private MockMultipartFile midataFile;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    uploadController = new UploadController(
+        mock(MidataCsvParser.class),
+        mock(BankingFacade.class));
+
+    String midataData = "Date,Type,Merchant/Description,Debit/Credit,Balance\n" +
+        "02/08/2016,DD,AAA BBBBB,-£50.00,+£50.00\n" +
+        "01/08/2016,VIS,CCCC ********** DDDDDD **********,-£10.00,+£40.00\n" +
+        "31/05/2016,CR,******************,+£1000.00,+£1040.00\n" +
+        "\n" +
+        "Arranged overdraft limit,06/09/2016,£0.00";
+
+    String mFile = "Midata.csv";
+    midataFile = new MockMultipartFile(mFile, mFile,"text/plain", midataData.getBytes());
+  }
+
+  @Test
+  public void testUploadTransactions() throws Exception {
+    assertTrue(uploadController.uploadTransactions(midataFile));
+  }
+
+}
