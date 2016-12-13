74fa329de43ab09c3e6ab0a24e7da271e0a1ce83
Manos <fragmanos@gmail.com>
2016-12-13 19:48:26 +0000

    Unnecessary logger removal

--- webApp/src/main/java/life/file/DateUtilsImpl.java
+++ webApp/src/main/java/life/file/DateUtilsImpl.java
@@ -1,20 +1,14 @@
 package life.file;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class DateUtilsImpl {
-
-  private static final Logger log = LoggerFactory.getLogger(DateUtilsImpl.class);
 
   private final String[] patterns = new String[]{"yyyy-MM-dd", "M/dd/yy", "M/d/yy", "MM/dd/yy", "MM/dd/yyyy", "dd/MM/yyyy"};
 
-  public LocalDate convertTextToDate(String date) throws ParseException {
+  public LocalDate convertTextToDate(String date) {
     for(String pattern : patterns) {
       try {
         return LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern));
